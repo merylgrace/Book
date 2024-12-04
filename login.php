382bcd14c96ee44751fba9782359963e1a6718b1
emerita <luntian429.21@gmail.com>
2024-12-04 22:58:07 +0800

    added the don't have an account

--- login.php
+++ login.php
@@ -36,3 +36,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <input type="password" name="password" required>
     <button type="submit">Login</button>
 </form>
+
+<p>Don't have an account? <a href="register.php">Register here</a>.</p>
